e0171eb1cfbe941dbc68386ab8e1b17f7a4fdff7
Basile2121 <basile.regnault@gmail.com>
2024-02-09 16:37:23 +0100

    Template Vidéo

---
+++ deps.ts
@@ -0,0 +1,3 @@
+export { serve } from "https://deno.land/std@0.166.0/http/server.ts";
+export { Server } from "https://deno.land/x/socket_io@0.2.0/mod.ts";
+export { Application, send, Context } from "https://deno.land/x/oak@v11.1.0/mod.ts";
